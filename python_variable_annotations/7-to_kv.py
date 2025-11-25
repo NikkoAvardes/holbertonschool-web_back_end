ff27434310243887eae0fa1c7d860b4d3615218b
NikkoAvardes <11460@holbertonstudents.com>
2025-11-25 20:35:52 +0100

    Correction de l'indentation et ajout de nouvelles lignes pour améliorer la lisibilité du code

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,5 +2,6 @@
 
 import typing
 
+
 def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
-	return k, v**2
+    return k, v**2
--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,7 +2,8 @@
 
 import typing
 
+
 def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
-	def make_function(x: float) -> float:
-		return x * multiplier
-	return make_function
+    def make_function(x: float) -> float:
+        return x * multiplier
+    return make_function

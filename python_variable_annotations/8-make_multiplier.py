867ce2a5be8247b4178fd558cbc57f0d8c1abd3b
NikkoAvardes <11460@holbertonstudents.com>
2025-11-25 20:21:32 +0100

    first commit

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import typing
+
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+	def make_function(x: float) -> float:
+		return x * multiplier
+	return make_function

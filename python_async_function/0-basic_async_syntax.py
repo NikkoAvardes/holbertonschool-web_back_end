55642809af93bbdf7bcb91349279cf3ff974fc2e
NikkoAvardes <11460@holbertonstudents.com>
2025-11-26 13:12:00 +0100

    Ajout de l'annotation de type pour le paramètre max_delay dans la fonction wait_random

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Attend un délai aléatoire compris entre 0 et max_delay secondes.
 

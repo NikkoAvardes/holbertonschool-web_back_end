bb4e22a661dbbdf86aecfd8103ce3e0e9157601b
NikkoAvardes <11460@holbertonstudents.com>
2025-11-26 17:03:58 +0100

    Ajout de la coroutine wait_n pour exécuter plusieurs attentes asynchrones et retour des délais

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Ce module fournit une coroutine pour exécuter plusieurs attentes asynchrones
+et retourner leurs délais.
+"""
+import asyncio
+import typing
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """
+    Lance n coroutines de wait_random et retourne la liste des délais obtenus.
+
+    Args:
+        n (int): Le nombre de coroutines à lancer.
+        max_delay (int): Le délai maximal pour chaque attente.
+
+    Returns:
+        List[float]: Liste des délais attendus pour chaque coroutine.
+    """
+    coroutine = [wait_random(max_delay) for _ in range(n)]
+    delay = []
+    for x in asyncio.as_completed(coroutine):
+        result = await x
+        delay.append(result)
+    return delay

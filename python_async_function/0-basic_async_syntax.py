cb0edd6676e317f8e00d2ae213053c2c546fc6ad
NikkoAvardes <11460@holbertonstudents.com>
2025-11-26 13:06:37 +0100

    Ajout de la fonction asynchrone wait_random pour attendre un délai aléatoire et création des fichiers principaux et README.md

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Ce module fournit une fonction asynchrone pour attendre un délai aléatoire.
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """
+    Attend un délai aléatoire compris entre 0 et max_delay secondes.
+
+    Args:
+        max_delay (int, optional): Le délai maximal en secondes. Par défaut 10.
+
+    Returns:
+        float: Le délai effectivement attendu en secondes.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay

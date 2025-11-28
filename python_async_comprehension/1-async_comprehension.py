f3209c6edfbf80ddbfb52d093cc656a79d09665e
NikkoAvardes <11460@holbertonstudents.com>
2025-11-28 10:43:39 +0100

    Ajout de l'annotation de type pour la fonction async_comprehension afin de préciser le type de retour et amélioration de la documentation.

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,15 +3,18 @@
 Ce module fournit une fonction qui utilise une compréhension asynchrone
 pour collecter les valeurs générées par async_generator.
 """
+from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
-    Collecte 10 nombres flottants générés de façon asynchrone par async_generator.
+    Collecte 10 nombres flottants générés de façon
+    asynchrone par async_generator.
 
     Returns:
-        list[float]: Liste des 10 nombres générés.
+        List[float]: Liste des 10 nombres générés.
     """
     result = [i async for i in async_generator()]
     return result

34aa5d42e99d136312fd6a1ea5fb39cbc669f58f
NikkoAvardes <11460@holbertonstudents.com>
2025-11-28 11:08:57 +0100

    Ajout des fichiers 2-main.py et 2-measure_runtime.py pour mesurer le temps d'exécution de la fonction async_comprehension en parallèle.

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Ce module mesure le temps d'exécution pour lancer 4 fois la fonction async_comprehension en parallèle.
+"""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Mesure le temps nécessaire pour exécuter 4 fois async_comprehension en parallèle.
+
+    Returns:
+        float: Durée totale d'exécution en secondes.
+    """
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end = time.time()
+    return end - start

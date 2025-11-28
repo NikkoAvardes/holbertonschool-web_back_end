9077f8f82e2ec1d96ea4c6c99610b0711b9f1182
NikkoAvardes <11460@holbertonstudents.com>
2025-11-28 10:40:14 +0100

    Mise à jour des fichiers 0-async_generator.py et 1-async_comprehension.py : suppression de code inutile et amélioration de la documentation.

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
-import asyncio
-
+"""
+Ce module fournit une fonction qui utilise une compréhension asynchrone
+pour collecter les valeurs générées par async_generator.
+"""
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension():
-	result = [i async for i in range(10)]
+    """
+    Collecte 10 nombres flottants générés de façon asynchrone par async_generator.
+
+    Returns:
+        list[float]: Liste des 10 nombres générés.
+    """
+    result = [i async for i in async_generator()]
+    return result

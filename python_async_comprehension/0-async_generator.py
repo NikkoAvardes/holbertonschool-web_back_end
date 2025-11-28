27d9233853cfffc66e80ea4864b14084853998e7
NikkoAvardes <11460@holbertonstudents.com>
2025-11-28 10:00:55 +0100

    Ajout des fichiers 0-async_generator.py et 0-main.py pour implémenter un générateur asynchrone produisant 10 nombres aléatoires avec une pause d'une seconde entre chaque génération.

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Ce module fournit un générateur asynchrone qui produit 10 nombres.
+entre 0 et 10, avec une pause d'une seconde entre chaque génération.
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Génère 10 nombres flottants aléatoires entre 0 et 10.
+
+    Yields:
+        float: Un nombre flottant aléatoire entre 0 et 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)

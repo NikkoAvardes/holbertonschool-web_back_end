2bfad908248c59c5917f54fee7794687eb72a57f
NikkoAvardes <11460@holbertonstudents.com>
2025-12-03 15:07:55 +0100

    Ajout du module pour mettre à jour les sujets des documents dans une collection MongoDB dans le fichier 10-update_topics.py

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module for updating document topics in MongoDB collection
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Updates topics of all school documents with specific name
+    
+    Args:
+        mongo_collection: pymongo collection object
+        name (str): school name to update
+        topics (list): list of topics approached in the school
+        
+    Returns:
+        UpdateResult: result of the update operation
+    """
+    fil = {"name": name}
+    update = {"$set": {"topics": topics}}
+    result = mongo_collection.update_many(fil, update)
+    return result

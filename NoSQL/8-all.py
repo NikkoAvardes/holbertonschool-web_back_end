f08330c2fc78136f04c1831ea88f2468db5cdb29
NikkoAvardes <11460@holbertonstudents.com>
2025-12-03 12:15:37 +0100

    Ajout de la documentation pour la fonction list_all dans le fichier 8-all.py

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
+"""
+Module for listing all documents in a MongoDB collection
+"""
+
+
 def list_all(mongo_collection):
+    """
+    Lists all documents in a collection
+
+    Args:
+        mongo_collection: pymongo collection object
+
+    Returns:
+        list: all documents in the collection, or empty list if no documents
+    """
     return list(mongo_collection.find())

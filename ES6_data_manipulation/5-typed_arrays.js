bbb88c7011da598adb8e6950f47d8f4a2c4d18b2
NikkoAvardes <11460@holbertonstudents.com>
2025-12-16 09:19:21 +0100

    Correction de la fonction createInt8TypedArray pour retourner un DataView au lieu d'un ArrayBuffer.

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer)
   if (position >= length) {
 	throw new Error('Position outside range');
   }
-  const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer)
   view[position] = value
-  return buffer
+  const dataView = new DataView(buffer)
+  return dataView
 }

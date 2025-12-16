12e4c0bf4bd3a1afc8353b8baf12d5a78cdba023
NikkoAvardes <11460@holbertonstudents.com>
2025-12-16 10:12:06 +0100

    Correction de la fonction hasValuesFromArray pour remplacer l'utilisation de isSubsetOf par une boucle de vérification, améliorant ainsi la logique de vérification.

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,10 @@
 export default function hasValuesFromArray(array, set) {
   const b_array = new Set(array);
   const b_set = new Set(set);
-  return b_set.isSubsetOf(b_array)
+  for (const elem of b_set) {
+    if (!b_array.has(elem)) {
+      return false;
+    }
+  }
+  return true;
 }

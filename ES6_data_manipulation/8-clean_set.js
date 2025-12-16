064c9c706dcf848f7fc2e606a09ee5a4faea36a7
NikkoAvardes <11460@holbertonstudents.com>
2025-12-16 13:48:16 +0100

    Correction de la fonction cleanSet pour gérer correctement les chaînes vides et améliorer la vérification des valeurs dans le Set.

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString == '') {
-	return ''
+  if (!startString || typeof startString !== 'string') {
+	return '';
   }
   return Array.from(set)
-  .filter(value => value.startsWith(startString))
+  .filter(value => value && value.startsWith(startString))
   .map(value => value.substring(startString.length))
   .join('-');
 }

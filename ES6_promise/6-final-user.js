35d09aa7194c4d95828a8f4b7b85908937b864b4
NikkoAvardes <11460@holbertonstudents.com>
2025-12-17 11:34:33 +0100

    Refactor la fonction handleProfileSignup pour retourner un tableau d'objets avec le statut et la valeur des promesses

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,11 @@
 import signUpUser from './4-user-promise.js'
 import uploadPhoto from './5-photo-reject.js'
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)]
+  return Promise.allSettled(promises).then((results) => {
+    return results.map(result => ({
+      status: result.status,
+      value: result.status === "fulfilled" ? result.value : result.reason,
+    }));
+  });
 }

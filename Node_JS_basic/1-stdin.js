201e98336a734d37f67ceb0bf652efc81048c23c
NikkoAvardes <11460@holbertonstudents.com>
2026-01-07 09:14:15 +0100

    Réécriture de la gestion des entrées utilisateur en utilisant process.stdin pour une meilleure interaction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,7 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+	process.stdout.write(`Your name is: ${data}`);
 });
-
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  readline.close();
-});
-readline.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+	process.stdout.write('This important software is now closing\n');
 });

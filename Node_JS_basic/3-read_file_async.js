d79fb710b36ff7702a6ba501d954307661661ad1
NikkoAvardes <11460@holbertonstudents.com>
2026-01-07 10:40:47 +0100

    Correction de l'indentation et amélioration de la lisibilité dans la fonction countStudents

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,35 +1,35 @@
 const fs = require('fs').promises;
 
 async function countStudents(path) {
-	try {
-		const data = await fs.readFile(path, 'utf-8');
-		const lines = data.split('\n').filter((line) => line.trim().length > 0);
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim().length > 0);
 
-		if (lines.length <= 1) {
-			throw new Error('Cannot load the database');
-		}
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
+    }
 
-		const students = lines.slice(1);
-		console.log(`Number of students: ${students.length}`)
+    const students = lines.slice(1);
+    console.log(`Number of students: ${students.length}`);
 
-		const fields = {};
-		for (const student of students) {
-			const parts = student.split(',');
-			const firstname = parts[0];
-			const field = parts[3];
+    const fields = {};
+    for (const student of students) {
+      const parts = student.split(',');
+      const firstname = parts[0];
+      const field = parts[3];
 
-			if (!fields[field]) {
-				fields[field] = [];
-			}
-			fields[field].push(firstname);
-		}
-		for (const field in fields) {
-			const list = fields[field];
-			console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
-		}
-		return Promise.resolve();
-	} catch (error) {
-		throw new Error('Cannot load the database');
-	}
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    }
+    for (const field in fields) {
+      const list = fields[field];
+      console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+    }
+    return Promise.resolve();
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 module.exports = countStudents;
